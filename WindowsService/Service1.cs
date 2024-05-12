81fa3cb8bafb76502a7945f9a9a00956e68c46e4
bugradostt <bugradostt@gmail.com>
2024-05-12 21:45:01 +0300

    Add Windows Service

---
+++ WindowsService/Service1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace WindowsService
+{
+    public partial class Service1 : ServiceBase
+    {
+        Timer veriketTimer = new Timer();
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        // Servis çalıştığında
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        // Servis duruduğunda
+        protected override void OnStop()
+        {
+        }
+        
+      
+    }
+}
